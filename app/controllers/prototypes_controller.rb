6570068bb366c74bad0bcb461c4324f92a88753c
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-16 18:14:19 +0900

    ストロングパラメーターの設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -8,4 +8,9 @@ class PrototypesController < ApplicationController
 
   def create
   end
+
+  private
+  def prototype_params
+    require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
 end
