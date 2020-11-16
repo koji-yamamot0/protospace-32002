de620f2ce6711d5c4e648912586c29baef25858d
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-16 19:04:26 +0900

    New Protoボタンをクリックすると新規投稿画面へ遷移するように変更、validatesのスペルミスの修正

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,7 +1,7 @@
 class Prototype < ApplicationRecord
   belongs_to :user
 
-  valedates :title,      presence: true
+  validates :title,      presence: true
   validates :catch_copy, presence: true
   validates :concept,    presence: true
   validates :image,      presence: true
