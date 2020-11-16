b59a4f45e9ac37eeea384843306ea9affb146b9c
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-16 17:49:14 +0900

    Prototypeテーブルのバリデーション設定

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,10 @@
 class Prototype < ApplicationRecord
   belongs_to :user
 
+  valedates :title,      presence: true
+  validates :catch_copy, presence: true
+  validates :concept,    presence: true
+  validates :image,      presence: true
+
   has_one_attached :image
 end
