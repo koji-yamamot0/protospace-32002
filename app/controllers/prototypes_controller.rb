edce20bf142862fa151df3fa501d2a21acd7bf33
koji-yamamot0 <becafo_0515@yahoo.co.jp>
2020-11-17 16:24:30 +0900

    ログインしているユーザーにのみ編集を許可

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -26,6 +26,7 @@ class PrototypesController < ApplicationController
   end
 
   def update
+    authenticate_user!
     prototype = Prototype.find(params[:id])
     if prototype.update(prototype_params)
       redirect_to prototype_path
